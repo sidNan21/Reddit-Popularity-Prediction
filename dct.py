06bce65d24ee7b16b38b34aed66eea69d764d1a7
Siddharth Nanda <sn9dq@virginia.edu>
2019-11-29 21:25:32 -0500

    z-scores for comment score and comment sentiment

--- dct.py
+++ dct.py
@@ -1,6 +1,7 @@
 # Capstone Data Collection Toop
 
-import os, json, sys, praw, afinn, spacy # pip install afinn spacy praw && python -m spacy download en
+import os, json, sys, praw, afinn, spacy # pip install afinn spacy praw scipy && python -m spacy download en
+from scipy.stats import zscore
 
 # Read-only reddit client
 reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
@@ -18,7 +19,7 @@ for submission in reddit.subreddit(subreddit).top(time):
     if count == num_posts: break # Terminate if count reached
     submission.comments.replace_more(limit=None) # No hierarchy for comments, analyze everything with equal importance
 
-    submissionList = list()
+    commentList, allScores, allSentiments = list(), list(), list()
     for comment in submission.comments.list():
         # Save required data from every comment
         element = {'created_utc': comment.created_utc, 'body': comment.body,
@@ -27,7 +28,15 @@ for submission in reddit.subreddit(subreddit).top(time):
         element['sentiment'] = afinn.score(comment.body)
         entities = spacy_nlp(comment.body).ents
         element['entities'] = [{item.label_: item.text} for item in entities]
-        submissionList.append(element)
+        # Save score and senitment to calculate z-score later
+        allScores.append(comment.score)
+        allSentiments.append(element['sentiment'])
+        # Save comment to commentList
+        commentList.append(element)
+
+    z_scores, z_sentiments = zscore(allScores), zscore(allSentiments)
+    for element, z_scr, z_snt in zip(commentList, z_scores, z_sentiments):
+        element['score_z-score'], element['sentiment_z-score'] = z_scr, z_snt
 
     with open('./data/' + submission.id + '.json', 'w', encoding='utf-8') as f:
         # Save required data from post itself
@@ -40,6 +49,6 @@ for submission in reddit.subreddit(subreddit).top(time):
         entities = spacy_nlp(submission.selftext).ents
         top['body_entities'] = [{item.label_: item.text} for item in entities]
         # Dump to json
-        json.dump([top, submissionList], f, ensure_ascii=False, indent=4)
+        json.dump([top, commentList], f, ensure_ascii=False, indent=4)
 
     count += 1

39dbb4fa8b10b8ce71d94d80fed5c97af7be5ce3
coryjk <cory.j.kim@gmail.com>
2020-02-22 12:15:37 -0500

    ignore all data upload except ones with 'example' in name

--- example.sh
+++
@@ -1,4 +0,0 @@
-echo "Example run of simple data collection script: serializes for the top 10 posts on r/nba this week"
-echo "Parameters: subreddit time num_posts"
-echo "Running: python3 dct.py nba week 10"
-python3 dct.py nba week 10

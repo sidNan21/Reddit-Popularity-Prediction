5d02fc0322fe562b8164261d073a43ede4b56d64
Cory Kim <cory.j.kim@gmail.com>
2020-04-10 23:45:33 -0400

    todo: track comment height/depth for prediction using ids

--- src/fetch.py
+++ src/fetch.py
@@ -30,33 +30,20 @@ def fetch_top_posts(sub, time):
 '''
 formats raw reddit data into serialized jsons with structure:
     data {
-            [
-                submission {
-                    <str>   id,
-                    <str>   title,
-                    <float> created_utc,
-                    <str>   body,
-                    <int>   score,
-                    <str> distinguished,
-                    comments [
-                        comment {
-                            <float> created_utc,
-                            <str>   body,
-                            <int>   score,
-                            <str>   distinguished,
-                            <bool>  parent
-                        }
-                        ..
-                    ]
-                }
-                ..
-            ]
+        comment {
+            <float> created_utc,
+            <str>   body,
+            <int>   score,
+            <int>   gilds,
+            <str>   distinguished,
+        },
+        ...
     }
 '''
 def flat_fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
-    # top reddit posts provided target sub and time
-    print('fetching posts from {0}...\n \
-           sorting by: {1}, scraping {2} posts, {3} comments each (depth={4})' \
+    # console message
+    print('fetching AND flattening comments from {0}...\
+           \nsorting by: {1}, scraping {2} posts, {3} comments each (depth={4})' \
            .format(sub, sort, num_posts, num_comments, depth))
     posts = fetch_top_posts(sub, time)[:num_posts]
 
@@ -80,6 +67,67 @@ def flat_fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
                 'distinguished' : comment.distinguished,
             }
             submission_list.append(comment_data)
+        percent = (iteration * (1+num_comments))/(num_posts + num_posts*num_comments) * 100
+        print("percent complete: " + str(percent) + "%")
+        iteration += 1
+
+    print('done!')
+    # serialize to json
+    return submission_list
+
+
+'''
+WIP: going to track comment depth level (by matching parent ids) THEN flatten
+'''
+def fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
+    # console message
+    print('fetching comments from {0}...\
+           \nsorting by: {1}, scraping {2} posts, {3} comments each (depth={4})' \
+           .format(sub, sort, num_posts, num_comments, depth))
+    posts = fetch_top_posts(sub, time)[:num_posts]
+
+    # things will REALLY break if depth < 0... check
+    assert depth >= 0, "depth field must be >= 0"
+
+    # list to build json
+    submission_list = list()
+    print('serializing data...')
+    iteration = 1
+    for submission in posts:
+        # depth=0: no hierarchy for comments
+        # depth=1: 1 child allowed
+        # depth=n: n children allowed
+        submission.comments.replace_more(limit=depth)
+        # organize comment data
+        comments = submission.comments.list()[:num_comments]
+        submission_data = {
+            'id'          : submission.id,
+            'created_utc' : submission.created_utc,
+            'comments'    : list(),
+        }
+        for c in comments:
+            comment_data = {
+                'id'            : c.id,
+                'parent_id'     : c.parent_id,
+                'created_utc'   : c.created_utc,
+                'body'          : c.body,
+                'score'         : c.score,
+                'gilds'         : c.gilded,
+                'distinguished' : c.distinguished,
+            }
+            submission_data['comments'].append(comment_data)
+        submission_list.append(submission_data)
+        # for comment in submission.comments[:num_comments]:
+        #     # basic comment data
+        #     comment_data = {
+        #         'created_utc'   : comment.created_utc,
+        #         'body'          : comment.body,
+        #         'score'         : comment.score,
+        #         'gilds'         : comment.gilded,
+        #         'distinguished' : comment.distinguished,
+        #     }
+        #     submission_list.append(comment_data)
+
         percent = (iteration * (1+num_comments))/(num_posts + num_posts*num_comments) * 100
         print("percent complete: " + str(percent) + "%")
         iteration += 1

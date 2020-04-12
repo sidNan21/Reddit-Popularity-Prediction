a0ead2611439286dd68aa564c84b63d0c93c502d
Cory Kim <cory.j.kim@gmail.com>
2020-04-12 15:21:21 -0400

    forest_fetch... adds depth data, abstracted fetching for all sort-by types

--- src/fetch.py
+++ src/fetch.py
@@ -6,8 +6,6 @@ fetch.py
         time - target time window (e.g. top x posts this week, month, ...)
         num_posts    - limit number of posts
         num_comments - limit number of comments
-    saves into a json file with simple naming convention:
-        {subreddit}_top{num_posts}{time}_t{ddmm-HHMM}.json
 '''
 
 import json
@@ -15,16 +13,27 @@ import praw
 import math
 import src.tools
 
-def fetch_controv_posts(sub, time):
+def praw_connect_and_fetch(sub, time, sort):
+    # check sort type
+    assert sort in ('controversial', 'new', 'hot', 'rising', 'top'), \
+        'invalid sort-by input, got: '.format(sort)
+    # check timeframe
+    assert time in ('all', 'day', 'hour', 'month', 'week', 'year'), \
+        'invalid time input, got: '.format(time)
     # read-only reddit client
     reddit = src.tools.redditclient()
-    posts = reddit.subreddit(sub).controversial(time)
-    return list(posts)
-
-def fetch_top_posts(sub, time):
-    # read-only reddit client
-    reddit = src.tools.redditclient()
-    posts = reddit.subreddit(sub).top(time)
+    subreddit = reddit.subreddit(sub)
+    if sort == 'controversial':
+        posts = subreddit.controversial(time)
+    elif sort == 'new':
+        posts = subreddit.new(time)
+    elif sort == 'hot':
+        posts = subreddit.hot(time)
+    elif sort == 'rising':
+        posts = subreddit.rising(time)
+    elif sort == 'top':
+        posts = subreddit.top(time)
+    # done
     return list(posts)
 
 '''
@@ -42,10 +51,10 @@ formats raw reddit data into serialized jsons with structure:
 '''
 def flat_fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
     # console message
-    print('fetching AND flattening comments from {0}...\
+    print('fetching flat comments from {0}...\
            \nsorting by: {1}, scraping {2} posts, {3} comments each (depth={4})' \
            .format(sub, sort, num_posts, num_comments, depth))
-    posts = fetch_top_posts(sub, time)[:num_posts]
+    posts = praw_connect_and_fetch(sub, time, sort)[:num_posts]
 
     # list to build json
     submission_list = list()
@@ -80,7 +89,7 @@ data {
     comment {
         <int>   id,         (*)
         <int>   parent_id,  (*)
-        <bool>  has_parent  (*)
+        <bool>  top_level   (*)
         <float> created_utc,
         <str>   body,
         <int>   depth,      (*)
@@ -91,14 +100,56 @@ data {
     ...
 }
 '''
-def forest_fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
+def forest_fetch(sub, time, sort='top', num_posts=0, num_comments=None, depth=None):
     # console message
     print('fetching comments from {0}...\
-           \nsorting by: {1}, scraping {2} posts, {3} comments each (depth={4})' \
-           .format(sub, sort, num_posts, num_comments, depth))
-    posts = fetch_top_posts(sub, time)[:num_posts]
+           \nsorting by: {1}, scraping {2} posts' \
+           .format(sub, sort, num_posts))
+    if num_comments:
+        print('hard limit of {0} comments per post...'.format(num_comments))
+    if depth:
+        print('with maximum comment tree depth of {0}...'.format(depth))
+    
+    # retrieve posts
+    posts = praw_connect_and_fetch(sub, time, sort)[:num_posts]
+
+    res = list()
+    depth_map = dict()
 
     # got messy... starting from scratch
+    for p in posts:
+        # each post has a hard limit of i < num_comments
+        i = 0
+        comments = p.comments.list()
+        for c in comments:
+            # limit reached
+            if not num_comments and i == num_comments:
+                break
+            # is top-level
+            if c.parent_id[:3] == 't3_':
+                depth_map[c.id] = 0
+            # not top-level
+            else:
+                depth_map[c.id] = depth_map[c.parent_id[3:]]+1
+            # enforce depth limit
+            if depth and depth_map[c.id] == depth:
+                break
+            # comment data
+            c_dat = {
+                'id'            : c.id,
+                'parent_id'     : c.parent_id,
+                'top_level'     : c.parent_id[:3] == 't3_',
+                'created_utc'   : c.created_utc,
+                'body'          : c.body,
+                'depth'         : depth_map[c.id],
+                'score'         : c.score,
+                'gilds'         : c.gilded,
+                'distinguished' : c.distinguished,
+            }
+            res.append(c_dat)
+            # track progress
+            i += 1
+            percent = math.floor(i/num_comments)
 
     # serialize to json
     return res

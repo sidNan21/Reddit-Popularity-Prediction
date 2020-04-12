a59fa5d6b9d1289745d5b5f26c584761f05d8dbd
Cory Kim <cory.j.kim@gmail.com>
2020-04-12 01:49:22 -0400

    restart forest_fetch...

--- src/fetch.py
+++ src/fetch.py
@@ -12,6 +12,7 @@ fetch.py
 
 import json
 import praw
+import math
 import src.tools
 
 def fetch_controv_posts(sub, time):
@@ -97,63 +98,7 @@ def forest_fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
            .format(sub, sort, num_posts, num_comments, depth))
     posts = fetch_top_posts(sub, time)[:num_posts]
 
-    # list to build json
-    comment_data = list()
-    top_level_comments = list() # will speed-up depth determination
-    print('serializing data...')
-    i = 1
-    for p in posts:
-        # depth=0: no hierarchy for comments
-        # depth=1: 1 child allowed
-        # depth=n: n children allowed
-        p.comments.replace_more(limit=depth)
-        # organize comment data
-        comments = p.comments.list()
+    # got messy... starting from scratch
 
-        ## TODO: BREAK WHEN NUM_COMMENTS REACHED!!!!
-
-        while comments:
-            c = comments.pop(0)
-            i += 1
-            c_data = {
-                'id'            : c.id,
-                'parent_id'     : c.parent_id,
-                'is_parent'     : len(c.replies) > 0,
-                'created_utc'   : c.created_utc,
-                'body'          : c.body,
-                'depth'         : 0,
-                'score'         : c.score,
-                'gilds'         : c.gilded,
-                'distinguished' : c.distinguished,
-            }
-            # is a top-level comment
-            if (c_data['parent_id'][:3] == 't3_'):
-                top_level_comments.append(c_data)
-            # append
-            comment_data.append(c_data)
-            for c_reply in c.replies:
-                comments.append(c_reply)
-
-        percent = (i * (1+num_comments))/(num_posts + num_posts*num_comments) * 100
-        print("percent complete: " + str(percent) + "%")
-
-    print('done fetching {0} comments'.format(i))
-    
-    # determine height level of each comment
-    res = list()
-    comments_queue = list(top_level_comments)
-    while comments_queue:
-        # get front, check children
-        comment = comments_queue.pop(0)
-        for c in comments_queue:
-            # found a child
-            if (comment['id'] == c['parent_id']):
-                c['depth'] = comment['depth'] + 1
-                # now must check their children
-                comments_queue.append(c)
-        # append popped comment
-        res.append(comment)
-
-    # determine height
     # serialize to json
     return res

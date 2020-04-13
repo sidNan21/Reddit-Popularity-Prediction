d5603a3d467cda3276d6a5b7a5a3cda6dde57ade
Siddharth Nanda <sn9dq@virginia.edu>
2020-04-12 21:50:34 -0400

    Consolidated fetch, collected data from r/relationship_advice

--- src/fetch.py
+++ src/fetch.py
@@ -40,6 +40,10 @@ def praw_connect_and_fetch(sub, time, sort):
 formats raw reddit data into serialized jsons with structure:
     data {
         comment {
+            <int>   id,
+            <int>   parent_id,
+            <bool>  top_level,
+            <int>   depth,
             <float> created_utc,
             <str>   body,
             <int>   score,
@@ -49,107 +53,58 @@ formats raw reddit data into serialized jsons with structure:
         ...
     }
 '''
-def flat_fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
+def fetch(sub, time, sort='top', num_posts=0, num_comments=0, depth=0):
     # console message
-    print('fetching flat comments from {0}...\
+    print('fetching comments from {0}...\
            \nsorting by: {1}, scraping {2} posts, {3} comments each (depth={4})' \
            .format(sub, sort, num_posts, num_comments, depth))
     posts = praw_connect_and_fetch(sub, time, sort)[:num_posts]
 
     # list to build json
-    submission_list = list()
+    comment_list = list()
     print('serializing data...')
-    iteration = 1
+    iteration = 0
     for submission in posts:
+        base_time = submission.created_utc
+        depth_store, comment_store = dict(), dict()
         # depth=0: no hierarchy for comments
         # depth=1: 1 child allowed
         # depth=n: n children allowed
         submission.comments.replace_more(limit=depth)
+        comment_queue = submission.comments[:]  # Seed with top-level
         # organize comment data
-        for comment in submission.comments[:num_comments]:
-            # basic comment data
+        while comment_queue:
+            comment = comment_queue.pop(0)
+            comment_queue.extend(comment.replies)
+            if comment.parent_id[:3] == 't3_':
+                depth_store[comment.id] = 0
+            else:
+                depth_store[comment.id] = depth_store[comment.parent_id[3:]]+1
             comment_data = {
+                'id'            : comment.id,
+                'parent_id'     : comment.parent_id,
+                'top_level'     : comment.parent_id[:3] == 't3_',
+                'depth'         : depth_store[comment.id],
                 'created_utc'   : comment.created_utc,
                 'body'          : comment.body,
                 'score'         : comment.score,
                 'gilds'         : comment.gilded,
                 'distinguished' : comment.distinguished,
             }
-            submission_list.append(comment_data)
-        percent = (iteration * (1+num_comments))/(num_posts + num_posts*num_comments) * 100
-        print("percent complete: " + str(percent) + "%")
+            comment_store[comment.id] = comment_data
+        '''for _id in comment_store:
+            comment = comment_store[_id]
+            diff = None
+            if comment['parent_id'][:3] == 't3_':
+                diff = comment['created_utc'] - base_time
+            else:
+                diff = comment['created_utc'] - comment_store[comment['parent_id']]['created_utc']
+            comment_store[_id]['time_diff'] = diff'''
+        comment_list += comment_store.values()
         iteration += 1
+        percent = (iteration)/(num_posts) * 100
+        print("percent complete: " + str(percent) + "%")
 
     print('done!')
     # serialize to json
-    return submission_list
-
-'''
-data {
-    comment {
-        <int>   id,         (*)
-        <int>   parent_id,  (*)
-        <bool>  top_level   (*)
-        <float> created_utc,
-        <str>   body,
-        <int>   depth,      (*)
-        <int>   score,
-        <int>   gilds,
-        <str>   distinguished,
-    },
-    ...
-}
-'''
-def forest_fetch(sub, time, sort='top', num_posts=0, num_comments=None, depth=None):
-    # console message
-    print('fetching comments from {0}...\
-           \nsorting by: {1}, scraping {2} posts' \
-           .format(sub, sort, num_posts))
-    if num_comments:
-        print('hard limit of {0} comments per post...'.format(num_comments))
-    if depth:
-        print('with maximum comment tree depth of {0}...'.format(depth))
-    
-    # retrieve posts
-    posts = praw_connect_and_fetch(sub, time, sort)[:num_posts]
-
-    res = list()
-    depth_map = dict()
-
-    # got messy... starting from scratch
-    for p in posts:
-        # each post has a hard limit of i < num_comments
-        i = 0
-        comments = p.comments.list()
-        for c in comments:
-            # limit reached
-            if not num_comments and i == num_comments:
-                break
-            # is top-level
-            if c.parent_id[:3] == 't3_':
-                depth_map[c.id] = 0
-            # not top-level
-            else:
-                depth_map[c.id] = depth_map[c.parent_id[3:]]+1
-            # enforce depth limit
-            if depth and depth_map[c.id] == depth:
-                break
-            # comment data
-            c_dat = {
-                'id'            : c.id,
-                'parent_id'     : c.parent_id,
-                'top_level'     : c.parent_id[:3] == 't3_',
-                'created_utc'   : c.created_utc,
-                'body'          : c.body,
-                'depth'         : depth_map[c.id],
-                'score'         : c.score,
-                'gilds'         : c.gilded,
-                'distinguished' : c.distinguished,
-            }
-            res.append(c_dat)
-            # track progress
-            i += 1
-            percent = math.floor(i/num_comments)
-
-    # serialize to json
-    return res
+    return comment_list
